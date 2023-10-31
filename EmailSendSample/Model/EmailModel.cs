a93fe46020eee847c33adbe74beeb6e5c83954e0
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-31 13:58:03 +0530

    works commited

--- EmailSendSample/Model/EmailModel.cs
+++ EmailSendSample/Model/EmailModel.cs
@@ -2,8 +2,8 @@
 {
     public class EmailModel
     {
-        public string ToEmail { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; } // You can add a property for the email body if needed
+        public string? ToEmail { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; } // You can add a property for the email body if needed
     }
 }
